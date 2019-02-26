671b592d8986194c562cd7fa12a1bcd275961d49
chaicj <chaicj@wopuwulian.com>
2019-02-26 09:49:35 +0800

    添加注释

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # concurrent
 java concurrent study
+
+1. synchronized(对象锁)-c_001
+2. synchronized(this)-c_002
+3. synchronized(方法)-c_003
+4. 
--- src/yxxy/c_001/T.java
+++ src/yxxy/c_001/T.java
@@ -1,7 +1,7 @@
 /**
  * synchronized关键字
  * 对某个对象加锁
- * @author mashibing
+ * @author jsjchai
  */
 
 package yxxy.c_001;
@@ -9,7 +9,7 @@ package yxxy.c_001;
 public class T {
 
     private int count = 10;
-    private Object o = new Object();
+    private final Object o = new Object();
 
     public void m() {
         //任何线程要执行下面的代码，必须先拿到o的锁
